9c90d3cd5a8e2cb7a3cb7bf4accfbb6a1e3b24d3
Fathin Naufaliya <76151903+fathin123@users.noreply.github.com>
2022-06-24 12:55:30 +0700

    perhitungan

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,6 @@ Route::prefix('/alternatif')->group(function ()
     Route::post('/hapus/{id}', 'AlternatifController@destroy')->name('alternatif.hapus');
 });
 
-
 //Route module for nilai
 Route::prefix('/nilai')->group(function ()
 {
